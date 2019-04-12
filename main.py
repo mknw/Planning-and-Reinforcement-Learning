e11a4f3bb4f3cdabcd8aaf232fbd8703f6bc1f3e
Flotju <49276379+Flotju@users.noreply.github.com>
2019-04-12 10:32:18 +0200

    Add files via upload
    Constants initialized

---
+++ main.py
@@ -0,0 +1,19 @@
+""" Group 2, Planning & Reinforcement Learning 2019
+    Ilze Amanda Auzina, Phillip Kersten,
+    Florence van der Voort, Stefan Wijtsma
+    Assignment Part 1"""
+
+
+RANDOM_POLICY_DISCOUNT_FACTOR   = 0.9
+SLIP_CHANCE                     = 0.05
+ACTION_PROBABILITY              = 0.95
+OUTSIDE_GRID_PROBABILITY        = 1
+
+ROBOT_STARTING_COORDINATES      = (3,0)
+SHIPWRECK_COORDINATES           = (2,2)
+GOAL_COORDINATES                = (0,3)
+CRACK_COORDINATE_LIST           = [(1,1), (1,3), (2,3), (3,1), (3,2), (3.3)]
+
+CRACK_REWARD                    = -10
+SHIPWRECK_REWARD                =  20
+GOAL_REWARD                     = 100
