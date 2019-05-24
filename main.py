1b2213fa4187b78e5d282d5851587ad96d04d564
Michael <mike.rac992@gmail.com>
2019-05-24 22:12:10 +0200

    boltzman instructions added

--- main.py
+++ main.py
@@ -10,7 +10,22 @@ import pandas as pd
 import env
 from learning import learning # sorry about that
 
+"""
+Usage notes:
 
+## Boltzmann exploration
+
+	- takes one more argument (taus) which can be a list or float (between 0 and 1)
+	examples:
+		q_boltzmann = learning('BOLTZMANN')
+		q_boltzmann(taus = [0.9, 0.5, 0.1], alpha, gamma, epsilon)
+	or:
+		q_boltzmann = learning('BOLTZMANN')
+		q_boltzmann(taus = 0.3, alpha, gamma, epsilon)
+
+## 
+
+"""
 
 if __name__ == "__main__":
 
@@ -18,7 +33,7 @@ if __name__ == "__main__":
 	q_learning = learning('BOLTZMANN')
 
 
-	q_learning(taus = [0.5], alpha = .1, gamma = .6, epsilon = .1) # here alpha gamma and epsilon can be overwritten
+	q_learning(taus = 0.5, alpha = .1, gamma = .6, epsilon = .1) # here alpha gamma and epsilon can be overwritten
 
 
 	import ipdb; ipdb.set_trace()
