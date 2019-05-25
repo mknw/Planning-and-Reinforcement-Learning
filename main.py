cbf4c053ad3a2c61fa57674631b9feac83434762
PhPeKe <p.kersten@live.de>
2019-05-25 14:14:25 +0200

    cleaned the plot function

--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 import numpy as np
 import random
 import pandas as pd
+from plot import plot
 import env
 import matplotlib as mpl
 import seaborn as sns
@@ -36,9 +37,10 @@ if __name__ == "__main__":
 	#methods = ["Q"]
 	methods = ["Q", "SARSA", "Q-ET", "Q-ER"]
 
-	masterplot = []
+	plot_dict = {}
 	all_all_plots = []
 	for method in methods:
+		plot_dict[method] = {}
 		q_learning = learning(method)
 		all_plots=[]
 
@@ -49,45 +51,12 @@ if __name__ == "__main__":
 
 
 		all_plots = np.array(all_plots)
-		all_all_plots.append(all_plots)
 		mean_plots = all_plots.mean(axis=0)
 		sd_plots = all_plots.mean(axis=0)
-		masterplot.append(mean_plots)
+		plot_dict[method]["all"] = all_plots
+		plot_dict[method]["mean"] = mean_plots
+		plot_dict[method]["sd"] = sd_plots
 		df = pd.DataFrame(mean_plots)
 
-		large = 22
-		med = 16
-		small = 12
-		params = {'axes.titlesize': large,
-		          'legend.fontsize': med,
-		          'figure.figsize': (16, 10),
-		          'axes.labelsize': med,
-		          'axes.titlesize': med,
-		          'xtick.labelsize': med,
-		          'ytick.labelsize': med,
-		          'figure.titlesize': large}
-		plt.rcParams.update(params)
-		plt.style.use('seaborn-whitegrid')
-		sns.set_style("white")
+		plot(mean_plots,method)
 
-		plt.figure(figsize=(16, 10), dpi=80)
-		plt.plot(range(len(mean_plots)), mean_plots, color='tab:red')
-
-		# Decoration
-		plt.ylim(0, 150)
-
-		xtick_labels = [str(x) for x in range(0,1001,50)]
-		xtick_location = [x for x in range(0,1001,50)]
-
-		plt.xticks(ticks=xtick_location, labels=xtick_labels, rotation=0, fontsize=12, horizontalalignment='center',
-		           alpha=.7)
-		plt.yticks(fontsize=12, alpha=.7)
-		plt.title(method, fontsize=22)
-		plt.grid(axis='both', alpha=.3)
-
-		# Remove borders
-		plt.gca().spines["top"].set_alpha(0.0)
-		plt.gca().spines["bottom"].set_alpha(0.3)
-		plt.gca().spines["right"].set_alpha(0.0)
-		plt.gca().spines["left"].set_alpha(0.3)
-		plt.show()
---
+++ plot.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def plot(mean_plots,method,mean=False):
+
+	large = 22
+	med = 16
+	small = 12
+	params = {'axes.titlesize': large,
+	          'legend.fontsize': med,
+	          'figure.figsize': (16, 10),
+	          'axes.labelsize': med,
+	          'axes.titlesize': med,
+	          'xtick.labelsize': med,
+	          'ytick.labelsize': med,
+	          'figure.titlesize': large}
+	plt.rcParams.update(params)
+	plt.style.use('seaborn-whitegrid')
+	sns.set_style("white")
+
+	plt.figure(figsize=(16, 10), dpi=80)
+	plt.plot(range(len(mean_plots)), mean_plots, color='tab:red')
+
+	# Decoration
+	plt.ylim(0, 150)
+
+	xtick_labels = [str(x) for x in range(0, 1001, 50)]
+	xtick_location = [x for x in range(0, 1001, 50)]
+
+	plt.xticks(ticks=xtick_location, labels=xtick_labels, rotation=0, fontsize=12, horizontalalignment='center',
+	           alpha=.7)
+	plt.yticks(fontsize=12, alpha=.7)
+	plt.title(method, fontsize=22)
+	plt.grid(axis='both', alpha=.3)
+
+	# Remove borders
+	plt.gca().spines["top"].set_alpha(0.0)
+	plt.gca().spines["bottom"].set_alpha(0.3)
+	plt.gca().spines["right"].set_alpha(0.0)
+	plt.gca().spines["left"].set_alpha(0.3)
+	plt.show()
